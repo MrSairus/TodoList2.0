264809ef37855337f46c3141df6f62cb78bcbac2
Ciro <201700251f@utea.edu.pe>
2021-06-26 23:43:47 -0500

    Se conecto el phpmyadmin con el archivo index para que el boton de save pueda registrar valores

--- index.php
+++ index.php
@@ -11,6 +11,40 @@
 </head>
 <body>
     <?php require_once 'process.php'; ?>
+    <div class="container">
+    <?php
+        $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+        //pre_r($result);
+        ?>
+
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+        <?php
+            while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['location']; ?></td>
+                    <td></td>
+                </tr>
+            <?php endwhile; ?>   
+            </table>
+        </div>
+        <?php
+
+        function pre_r( $array ) {
+            echo '<pre>';
+            print_r($array);
+            echo '</pre';
+        }
+    ?>
     <div class="row justify-content-center">
     <form action="process.php" method="POST">
         <div class="form-group">
@@ -25,6 +59,7 @@
         <button type="submit" class="btn btn-primary" name="save">Save</button>
         </div>        
     </form>
+    </div>
     </div>
 </body>
 </html>
